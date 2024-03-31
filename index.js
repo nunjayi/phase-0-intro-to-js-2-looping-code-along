0348fbf9a1870919c257af98c8c81837c7dbc979
nunjayi <epenyash@gmail.com>
2024-03-31 21:31:15 +0300

    solving loops using while and for loop

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(nameArray,event){
+    let messagesArray = []
+    for(let i=0;i<=nameArray.length-1;i++){
+        messagesArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return messagesArray
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+function countDown(number){
+    while(number>=0){
+        console.log(number)
+        number--
+    }
+
+}
+countDown(15)
